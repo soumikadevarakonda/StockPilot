c887b298379f116e4206a3403348b6b836b385bc
Soumika Devarakonda <142423331+soumikadevarakonda@users.noreply.github.com>
2026-01-16 13:31:33 +0530

    Update axios baseURL to new port

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:30083/api", // your Spring Boot base
+  baseURL: "http://localhost:8080/api", // your Spring Boot base
 });
 
 export default instance;
